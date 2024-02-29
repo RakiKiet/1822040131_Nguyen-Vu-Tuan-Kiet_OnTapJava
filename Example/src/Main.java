cd63507c27b22df7854a851310095104c14e7ded
RakiKiet <kiet121004@gmail.com>
2024-02-29 18:43:59 +0700

    java

---
+++ Example/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        ToanCoBan t1 = new ToanCoBan(2.5, 5.7, 11.2);
+
+        System.out.println("a = " + t1.getA());
+        System.out.println("b = " + t1.getB());
+        System.out.println("c = " + t1.getC());
+        System.out.println("Tổng = " + t1.tong());
+    }
+}
